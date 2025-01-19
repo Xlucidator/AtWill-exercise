6912f8a85e0a8c825a9bf7ee3abf706cdb656fae
Xlucidator <rubixue572@gmail.com>
2025-01-19 19:17:06 +0800

    pass 3510: specual lcs, using lis

--- acwings/lesson-advanced/dp/lcs/3510.cpp
+++ acwings/lesson-advanced/dp/lcs/3510.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+#define USE_VECTOR
 
 const int MAXN = 1e6+2;
 
 int q[MAXN];
-int dp_seq[MAXN]; // LIS
 int hash_idx[MAXN]; // hash index of a
 int n;
+
+// LIS
+#ifdef USE_VECTOR
+    vector<int> dp_seq; // to use lower_bound
+#else 
+    int dp_seq[MAXN]; 
+    int len;
+#endif
 
 int main() {
     scanf("%d", &n);
@@ -19,11 +29,40 @@ int main() {
         scanf("%d", &tmp);
         hash_idx[tmp] = i;
     }
-    /* Read b[]: translate to q[] */
+    /* Read b[]: translate to LIS q[] */
     for (int i = 0; i < n; ++i) {
         scanf("%d", &tmp);
         q[i] = hash_idx[tmp]; // character out of a[] would be 0
     }
+
+
+    for (int i = 0; i < n; ++i) {
+        if (q[i] == 0) continue;
+        int target = q[i];
+    #ifdef USE_VECTOR
+        auto it = lower_bound(dp_seq.begin(), dp_seq.end(), target);
+        if (it == dp_seq.end()) dp_seq.push_back(target);
+        else *it = target;
+    #else 
+        if (len == 0 || target > dp_seq[len-1]) dp_seq[len++] = target;
+        else { // target <= dp_seq[len-1]
+            int lo = 0, hi = len - 2, mi;
+            while (lo <= hi) {
+                mi = (lo + hi) >> 1;
+                if (target <= dp_seq[mi]) hi = mi - 1;
+                else lo = mi + 1;
+            }
+            dp_seq[lo] = target;
+        }
+    #endif
+    }
+
+#ifdef USE_VECTOR
+    printf("%d\n", dp_seq.size());
+#else 
+    printf("%d\n", len);
+#endif
+    
     
     return 0;
 }

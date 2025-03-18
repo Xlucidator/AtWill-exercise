3f730b0f00fb0ce0897d3ee3c96b4da9a6d3fe1f
Xlucidator <rubixue572@gmail.com>
2025-03-18 23:02:17 +0800

    pass 901, memorized-search for dp, recursive with memo

---
+++ acwings/lesson-advanced/dp/memorized-search/901.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+const int N = 310;
+
+int n, m;
+int h[N][N];
+int dp[N][N];
+
+// up right down left
+const int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+inline bool inbound(int x, int y) { 
+    return 1 <= x && x <= n && 1 <= y && y <= m;
+}
+
+int dpRecursive(int x, int y) {
+    int& v = dp[x][y];
+    if (v != 0) return v;
+    // printf("[trace] (%d, %d)\n", x, y);
+
+    int a, b;
+    for (int i = 0; i < 4; ++i) {
+        a = x + dx[i], b = y + dy[i];
+        if (inbound(a, b) && h[a][b] < h[x][y])
+            v = max(v, dpRecursive(a, b) + 1);
+    }
+    return v = max(v, 1);
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; ++i) 
+        for (int j = 1; j <= m; ++j) 
+            scanf("%d", &h[i][j]);
+            
+    // memset(dp, -1, sizeof dp);
+    int res = 0;
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= m; ++j)
+            res = max(res, dpRecursive(i, j));
+    printf("%d\n", res);
+
+    return 0;
+}
